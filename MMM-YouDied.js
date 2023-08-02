ed0aca32238792bb87014cf005d500cab2d9ea19
hazm8 <harrybrandon0@gmail.com>
2023-08-02 16:15:04 +1000

    Add files via upload

---
+++ MMM-YouDied.js
@@ -0,0 +1,26 @@
+
+// MMM-YouDied.js:
+
+Module.register("MMM-YouDied", {
+    //default module config
+    defaults: {
+
+
+    }
+
+    //overide dom generator
+    getDom: function (){
+
+            var imgage = document.createElement('div');
+            wrapper.className
+
+
+        
+    }
+
+
+
+
+
+
+});
